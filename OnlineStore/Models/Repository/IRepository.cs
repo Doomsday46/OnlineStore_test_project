cc9306d1ec95bd68f5486bf9044d9371735195c8
Vitaliy <Gallifrey46@gmail.com>
2018-10-16 11:51:30 +0300

    Добавление шаблона LinqToSql

--- OnlineStore/Models/Repository/IRepository.cs
+++ OnlineStore/Models/Repository/IRepository.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using OnlineStore.Models.Entity;
 
 namespace OnlineStore.Models
 {
-    public interface IRepository
+    public interface IRoleRepository
     {
         IQueryable<Role> Roles { get; }
 
