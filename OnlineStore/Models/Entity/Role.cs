a5fc259b8e23845b4aaa39ca6d22d7c81a511e46
Vitaliy <Gallifrey46@gmail.com>
2018-10-16 11:05:24 +0300

    Создан интерфейс репозитория для сущности Role

---
+++ OnlineStore/Models/Entity/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Models.Entity
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ OnlineStore/Models/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Role> Roles { get; }
+
+        bool CreateRole(Role instance);
+
+        bool UpdateRole(Role instance);
+
+        bool RemoveRole(int idRole);
+    }
+}
